acd16f579180c2b3d116836e189fdb1c9b8de340
alexvc11 <61017171+alexvc11@users.noreply.github.com>
2020-11-13 11:58:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const form = document.getElementById("formulario")
 
 form.onsubmit = (e) => {
+    const tit = document.getElementById("título");
+    const titval = tit.value();
+    const desc = document.getElementById("descripción"):
+    const decscval = desc.value();
+    const name = document.getElementById("nombre");
+    const nameval = name.value();
+    
+    document.alert(titval);
     e.preventdefault();
     
 }
